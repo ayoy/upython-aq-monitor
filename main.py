39e0797047cf8cf120d5c374ea448cdb1b13c6b3
Dominik Kapusta <dominik@kapusta.cc>
2017-11-16 23:01:21 +0100

    Use deepsleep

--- main.py
+++ main.py
@@ -36,59 +36,60 @@ uart.deinit()
 
 chrono = Timer.Chrono()
 
-while True:
-    chrono.start()
-    en.value(1)
-    rst.value(1)
-    uart.init(pins=(Pin.exp_board.G14, Pin.exp_board.G15))
-    printt('waiting 10s to take measurement')
-    machine.idle()
-    time.sleep_ms(10000)
+chrono.start()
+en.value(1)
+rst.value(1)
+uart.init(pins=(Pin.exp_board.G14, Pin.exp_board.G15))
+printt('waiting 10s to take measurement')
+machine.idle()
+time.sleep_ms(10000)
+u = uart.any()
+printt('waiting for data {}'.format(str(u)))
+while u < 32:
     u = uart.any()
-    printt('waiting for data {}'.format(str(u)))
-    while u < 32:
-        u = uart.any()
-        pycom.rgbled(0x550000)
-    printt('data ready {}'.format(str(uart.any())))
-    pycom.rgbled(0x000000)
+    pycom.rgbled(0x550000)
+printt('data ready {}'.format(str(uart.any())))
+pycom.rgbled(0x000000)
 
-    frame_read = False
+frame_read = False
 
-    while frame_read == False:
-        while uart.read(1) != b'\x42':
-            machine.idle()
+while frame_read == False:
+    while uart.read(1) != b'\x42':
+        machine.idle()
 
-        if uart.read(1) == b'\x4D':
-            while u < 32:
-                u = uart.any()
-                pycom.rgbled(0x550000)
+    if uart.read(1) == b'\x4D':
+        while u < 32:
+            u = uart.any()
+            pycom.rgbled(0x550000)
 
-            pycom.rgbled(0x000000)
-            (cpm25, cpm10, pm25, pm10) = read_frame(b'\x42\x4D' + uart.read(30))
-            if (cpm25, cpm10, pm25, pm10) == (-1, -1, -1, -1):
-                printt('error reading frame, skipping {}'.format(uart.any()))
-            else:
-                frame_read = True
-                t = utime.localtime(utime.time())
-                timestamp = '{}-{:02d}-{:02d} {:02d}:{:02d}:{:02d}'.format(t[0], t[1], t[2], t[3], t[4], t[5])
-                printt('{0} cPM25: {1}, cPM10: {2}, PM25: {3}, PM10: {4}'.format(timestamp, cpm25, cpm10, pm25, pm10))
+        pycom.rgbled(0x000000)
+        (cpm25, cpm10, pm25, pm10) = read_frame(b'\x42\x4D' + uart.read(30))
+        if (cpm25, cpm10, pm25, pm10) == (-1, -1, -1, -1):
+            printt('error reading frame, skipping {}'.format(uart.any()))
+        else:
+            frame_read = True
+            t = utime.localtime(utime.time())
+            timestamp = '{}-{:02d}-{:02d} {:02d}:{:02d}:{:02d}'.format(t[0], t[1], t[2], t[3], t[4], t[5])
+            printt('{0} cPM25: {1}, cPM10: {2}, PM25: {3}, PM10: {4}'.format(timestamp, cpm25, cpm10, pm25, pm10))
 
-                influx_url = 'http://rpi.local:8086/write?db=mydb'
-                data = 'aqi,indoor=1 pm25={},pm10={}'.format(pm25, pm10)
-                try:
-                    r = urequests.post(influx_url, data=data)
-                    pycom.rgbled(0x000055)
-                    time.sleep_ms(20)
-                    pycom.rgbled(0x000000)
+            influx_url = 'http://rpi.local:8086/write?db=mydb'
+            data = 'aqi,indoor=1 pm25={},pm10={}'.format(pm25, pm10)
+            try:
+                r = urequests.post(influx_url, data=data)
+                pycom.rgbled(0x000055)
+                time.sleep_ms(20)
+                pycom.rgbled(0x000000)
 
-                    en.value(0)
-                    uart.deinit()
-                    printt('sleeping for 10 mins {}'.format(str(uart.any())))
-                    chrono.stop()
-                    chrono.reset()
-                    machine.idle()
-                    time.sleep(600 - chrono.read())
-                    # time.sleep(5)
-                except OSError as e:
-                    printt('network error: {}'.format(e.errno))
-                    pass
+                en.value(0)
+                uart.deinit()
+                printt('sleeping for 10 mins {}'.format(str(uart.any())))
+                chrono.stop()
+                elapsed_ms = int(chrono.read()*1000)
+                chrono.reset()
+                machine.deepsleep(600*1000 - elapsed_ms)
+                # time.sleep(600 - chrono.read())
+                # machine.deepsleep(5*1000)
+                # time.sleep(5)
+            except OSError as e:
+                printt('network error: {}'.format(e.errno))
+                pass
