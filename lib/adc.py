3f1d1d87906f01e4244d0e2a8f99811061499493
Dominik Kapusta <dominik@kapusta.cc>
2017-11-22 10:23:47 +0100

    Fix ADC readings

--- lib/adc.py
+++ lib/adc.py
@@ -1,9 +1,12 @@
 import machine
 numADCreadings = const(100)
+adcVmax = 10**(3/20)
+
 def ADCloopMeanStdDev():
     adc = machine.ADC(0)
     adcread = adc.channel(attn=1, pin='P16')
-    samplesADC = [0.0]*numADCreadings; meanADC = 0.0
+    samplesADC = [0.0]*numADCreadings
+    meanADC = 0.0
     i = 0
     while (i < numADCreadings):
         adcint = adcread()
@@ -11,13 +14,13 @@ def ADCloopMeanStdDev():
         meanADC += adcint
         i += 1
     meanADC /= numADCreadings
-    varianceADC = 0.0
-    for adcint in samplesADC:
-        varianceADC += (adcint - meanADC)**2
-    varianceADC /= (numADCreadings - 1)
+    # varianceADC = 0.0
+    # for adcint in samplesADC:
+    #     varianceADC += (adcint - meanADC)**2
+    # varianceADC /= (numADCreadings - 1)
     # print("%u ADC readings :\n%s" %(numADCreadings, str(samplesADC)))
     # print("Mean of ADC readings (0-4095) = %15.13f" % meanADC)
-    # print("Mean of ADC readings (0-3700 mV) = %15.13f" % (meanADC*3700/4096))
+    # print("Mean of ADC readings (0-1412 mV) = %15.13f" % (meanADC*(adcVmax)/4096))
     # print("Variance of ADC readings = %15.13f" % varianceADC)
     # print("10**6*Variance/(Mean**2) of ADC readings = %15.13f" % ((varianceADC*10**6)//(meanADC**2)))
-    return meanADC*3700/4096
+    return (meanADC*adcVmax/4096)/(56/171)*1000
