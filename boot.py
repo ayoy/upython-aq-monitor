9c585ebb7b228f95af8698e51f59f5e59d01b1c8
Dominik Kapusta <dominik@kapusta.cc>
2018-01-16 13:38:55 +0100

    Update for LoPy4: change PMS5003 pins and remove LED PWM code

--- boot.py
+++ boot.py
@@ -24,8 +24,8 @@ watchdog_timer = None
 if machine.reset_cause() not in [machine.DEEPSLEEP_RESET, machine.SOFT_RESET, machine.WDT_RESET]:
     pycom.nvs_erase_all()
     persistence.cleanup()
-# else:
-    # watchdog_timer = WDT(timeout=30000)
+else:
+    watchdog_timer = WDT(timeout=30000)
 
 
 machine.main('main.py')
